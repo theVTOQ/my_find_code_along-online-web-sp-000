1e4d8be8865a5e6c1f6dcc3cbb5f0293a0220907
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2018-11-11 18:18:55 +0000

    automatically backed up by learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -2,8 +2,14 @@ require 'pry'
 
 def my_find(collection)
   i = 0
+  matches = []
   while i < collection.length
-
+    entry = collection[i]
+    valid? = yield(entry)
+    if valid?
+      matches <<  entry
+    end
     i += 1
   end
+  matches
 end
